cf7e1563ddff295c921d70341e1078f67a3e432d
Elisha Nam <namelisha@gmail.com>
2023-07-24 17:24:42 -0700

    Added ripple effect to buttons in home page

--- app/Blog.tsx
+++ app/Blog.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Ripple, initTE } from "tw-elements";
-
-initTE({ Ripple });
+import { Button } from "@material-tailwind/react";
 
 interface BlogProps {
   title: string;
@@ -11,18 +9,18 @@ interface BlogProps {
 
 function Blog({ title, author }: BlogProps) {
   return (
-    <button
-      //   data-ripple-dark="true"
-      //   className="bg-black px-8 py-4 my-4 mx-4 text-left hover:shadow-lg hover:transition-shadow active:shadow-none disabled:shadow-none"
-      className="middle none center mr-3 rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      data-te-ripple-init
-      data-te-ripple-color="light"
+    <Button
+      variant="text"
+      color="white"
+      className="flex flex-col px-8 py-4 my-4 mx-4 text-left hover:shadow-lg"
     >
-      <div className="text-[#f1356d] font-quicksand font-bold text-lg">
+      <span className="text-[#f1356d] font-quicksand font-bold text-lg">
         {title}
-      </div>
-      <div className="font-sans-serif font-light">Written by: {author}</div>
-    </button>
+      </span>
+      <span className="text-black font-sans-serif font-light">
+        Written by: {author}
+      </span>
+    </Button>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -17,5 +22,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tw-elements/dist/plugin.cjs")]
-}
+  plugins: []
+});
