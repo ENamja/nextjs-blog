9340585c58cb46e63b396b6293d1d7cedac38d37
Elisha Nam <namelisha@gmail.com>
2023-07-29 22:40:40 -0700

    Removed unused import in blog/[id]/page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 import Blog from "./Blog";
 
 function BlogPage({ params }: { params: { id: string } }) {
-  const [aToZ, setAToZ] = useState(true);
-
   return <Blog id={params.id}></Blog>;
 }
 
